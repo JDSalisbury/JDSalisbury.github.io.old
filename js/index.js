de7dc6902f1e70bd92f75fb592643b94623d49d8
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-04-26 22:40:16 -0400

    drop menu gone

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 // Add a button in your navigation that will toggle the
 // visibility of your actual site navigation.
 
-const clickMenu = document.querySelector('.drop__button');
+// const clickMenu = document.querySelector('.drop__button');
 
-clickMenu.addEventListener('click', function() {
-  const menu = document.querySelector('nav ul');
-  if (menu.style.display === "block") {
-    menu.style.display = 'none';
-  } else {
-    menu.style.display = 'block';
-  };
-});
+// clickMenu.addEventListener('click', function() {
+//   const menu = document.querySelector('nav ul');
+//   if (menu.style.display === "block") {
+//     menu.style.display = 'none';
+//   } else {
+//     menu.style.display = 'block';
+//   };
+// });
 
 // Create a modal box that pops up with your bio
 // information whenever a button or link is pressed
@@ -32,3 +32,6 @@ function windowOnClick(event) {
 aboutMe.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
+
+
+// window.addEventListener("hashchange", function() { scrollBy(0, -75) })
